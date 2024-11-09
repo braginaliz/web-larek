4166e4cac068cdd20fd29abef6518e4f688fc9a4
braginaliz <158730026+braginaliz@users.noreply.github.com>
2024-11-09 22:46:18 +0300

    Update index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,35 @@
+export type PaymentTypes = 'cash' | 'card';
+
+export interface ProductDetails {
+    id: string;                  // уникальный идентификатор товара
+    name: string;                // наименование товара
+    price: number | null;        // цена товара или null
+    description: string;         // описание товара
+    category: string;            // категория товара
+    image: string;               // URL изображения товара
+}
+
+export interface OrderDetails {
+    payment: PaymentTypes;       // способ оплаты
+    email: string;               // email клиента
+    phone: string;               // телефон клиента
+    address: string;             // адрес доставки
+    total: number;               // общая сумма заказа
+    items: string[];             // идентификаторы заказанных товаров
+}
+
+export interface ShoppingBasket {
+    items: string[];             // список идентификаторов товаров
+    total: number;               // общая стоимость всех товаров
+}
+
+export type OrderInput = Omit<OrderDetails, 'total' | 'items'> & {
+    valid: boolean;              // свойство, указывающее на валидность данных
+    errors: string[];           // массив ошибок валидации
+};
+
+export interface OrderOutcome {
+    id: string;                  // идентификатор заказа
+    total: number;               // общая сумма заказа
+}
+
